5931324fb5f13f1b3ac577518733fa1561d1f5b7
Shaw Kagawa <skagawa9@hotmail.com>
2016-11-24 09:43:33 -0800

    "First commit of completed week 2 prog assignment files"

---
+++ R Programming/Week 2 Prog Assignment/corr.R
@@ -0,0 +1,16 @@
+corr <- function(directory, threshold = 0){
+        fileList <- list.files(path = directory, pattern = ".csv", full.names = TRUE)
+        values <- numeric()
+        
+        for(i in 1:332){
+                data <- read.csv(fileList[i])
+                nobs <- sum(complete.cases(data))
+                if(nobs > threshold){
+                        values <- c(values, cor(data[, "nitrate"], data[, "sulfate"], use = "complete.obs"))
+                }
+        }
+        values
+}
+
+cr <- corr("specdata", 150)
+head(cr)
